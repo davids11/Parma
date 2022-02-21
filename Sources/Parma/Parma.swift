827f3284d98016f3731de5faad70b36abdcaae58
David Scheutz <scheutz.david@gmail.com>
2022-02-21 11:17:24 -0500

    Initial Commit

---
+++ Sources/Parma/Parma.swift
@@ -0,0 +1,6 @@
+public struct Parma {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/ParmaTests/ParmaTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Parma
+
+final class ParmaTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Parma().text, "Hello, World!")
+    }
+}
